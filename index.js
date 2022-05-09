2e7070d626e823f7d1c00fbde7054703c19dda8d
Roger Romaniczen <rogerdr.rdr@gmail.com>
2022-05-09 13:21:01 -0300

    formatando-texto

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ var botaoMenu = document.querySelector(".cabecalho__menu");
 var botaoFechar = document.querySelector(".barra-lateral__close");
 var spanSombra = document.querySelector(".conteudo-ofuscado");
 var barraNav = document.querySelector(".barra-lateral");
-spanSombra.classList.toggle("conteudo-ofuscado");
 
 botaoMenu.addEventListener("click", function(){
     barraNav.classList.remove("barra-lateral--oculta");
-    spanSombra.classList.toggle("conteudo-ofuscado");
 })
 botaoFechar.addEventListener("click", function(){
     barraNav.classList.add("barra-lateral--oculta");
-    spanSombra.classList.toggle("conteudo-ofuscado");
 })
