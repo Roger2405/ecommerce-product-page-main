29c18dba4bd36b8b6cc7c099b5e5e5c7c2be08e8
Roger Romaniczen <rogerdr.rdr@gmail.com>
2022-05-11 19:12:49 -0300

    melhorando-lightbox

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var botaoMenu = document.querySelector(".cabecalho__menu");
 var botaoFechar = document.querySelector(".barra-lateral__close");
 var spanSombra = document.querySelector(".conteudo-ofuscado");
 var barraNav = document.querySelector(".barra-lateral");
+var lightbox = document.querySelector(".lightbox");
 
 botaoMenu.addEventListener("click", function(){
     barraNav.classList.remove("barra-lateral--oculta");
@@ -12,17 +13,27 @@ botaoFechar.addEventListener("click", function(){
     barraNav.classList.add("barra-lateral--oculta");
 })
 
-var imagens = document.querySelectorAll(".thumbnail");
 
 var imagemPrincipal = document.querySelector(".imagem-principal");
-var imagensDestaque = document.querySelector(".imagens-destaque");
 imagemPrincipal.addEventListener("click", function(){
-    imagensDestaque.classList.toggle("imagens-destaque-ativo");
+    lightbox.classList.add("lightbox-ativo");
+});
+var botaoFechar = document.querySelector(".botao-fechar");
+botaoFechar.addEventListener("click", function(){
+    lightbox.classList.remove("lightbox-ativo");
 })
+
+var imagens;
+var buscaPorLightbox = document.querySelector(".lightbox-ativo");
+if(buscaPorLightbox != null){
+    imagens = document.querySelectorAll(".thumbnail-lightbox");
+}
+else {
+   imagens = document.querySelectorAll(".thumbnail");
+}
 
 imagens.forEach(imagem => {
     imagem.addEventListener("click", function(){
-        var imagemPrincipal = document.querySelector(".imagem-principal");
         var imagemAtivaAntiga = document.querySelector(".imagem-ativa");
         imagemAtivaAntiga.classList.remove("imagem-ativa");
         imagem.classList.toggle("imagem-ativa");
