fc14535411b1c6992263a1bd700885c597a6ab62
Roger <rogerdr.rdr@gmail.com>
2022-08-20 21:59:57 -0300

    iniciando funcionalidade de mudar imagens pelo botao

--- js/changeImages.js
+++ js/changeImages.js
@@ -1,9 +1,12 @@
 const rightButton = document.querySelector('#rightButton');
 const leftButton = document.querySelector('#leftButton');
-var imagemPrincipal = document.querySelector(".imagem-principal");
-var imagemPrincipalLightbox = document.querySelector('.imagem-principal-lightbox');
+const imagemPrincipal = document.querySelector(".imagem-principal");
+const imagemPrincipalLightbox = document.querySelector('.imagem-principal-lightbox');
+var imagemPrincipalSrc = imagemPrincipal.src;
 
 var imagens = document.querySelectorAll(".thumbnail");
+
+//checkButtons();
 
 imagens.forEach(imagem => {
     imagem.addEventListener("click", () => {
@@ -14,13 +17,86 @@ imagens.forEach(imagem => {
 
         srcNovaImagem = srcImagemAtiva.replace("-thumbnail", "");
         
-        imagemPrincipal.src = srcNovaImagem;
-        imagemPrincipalLightbox.src = srcNovaImagem;
+        refreshImage(srcNovaImagem);
     })
 });
 
+function refreshImage(newSrc) {
+    imagemPrincipalSrc = newSrc;
+    imagemPrincipal.src = newSrc;
+    imagemPrincipalLightbox.src = newSrc;
+    //checkButtons();
+}
+
 rightButton.addEventListener('click', () => {
-    var scr = imagemPrincipal.src;
-    //var newSrc = src.
+    console.log('right');
+    newSrc(idImagem() + 1);
+});
+leftButton.addEventListener('click', () => {
+    console.log('left')
+    newSrc(idImagem() - 1);
 })
 
+
+async function imageValid(src) {
+    setTimeout(() => {
+        let isValid = false;
+        fetch(new Request(src, {method: 'GET', mode: 'no-cors'}))
+        .then(response => {
+            if(response.status == 200) {
+                isValid = true;
+            }
+            else {
+                isValid = false;
+            }
+        })
+        return isValid;
+    }, 500);
+}
+
+async function newSrc(num) {
+    
+    var newSrc = mountUrl(num);
+    var isImgValid = await imageValid(newSrc);
+    
+    console.log("imagem é válida: " + isImgValid);
+    
+    if(isImgValid) {
+        refreshImage(newSrc);
+    }
+    
+}
+function mountUrl(id) {
+    const firstSubstr = imagemPrincipalSrc.slice(0, -5);
+    const secondSubstr = imagemPrincipalSrc.slice(-4);
+    
+    var src = firstSubstr + id + secondSubstr;
+
+
+    return src;
+}
+
+function checkButtons() {
+    const nextImage = mountUrl(idImagem() + 1);
+    const previousImage = mountUrl(idImagem() - 1);
+
+    if(!imageValid(nextImage)) {
+        rightButton.classList.add('hide');
+    }
+    else {
+        rightButton.classList.remove('hide');
+    }
+
+    if(!imageValid(previousImage)) {
+        leftButton.classList.add('hide');
+    }
+    else {
+        leftButton.classList.remove('hide');
+    }
+}
+
+function idImagem() {
+    var numStr = imagemPrincipalSrc.slice(-5, -4);
+    var num = Number.parseInt(numStr);
+    return num;
+}
